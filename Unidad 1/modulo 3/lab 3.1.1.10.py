903bbe439987be19649b14ea2ab6cdcf949451aa
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-10-02 09:52:43 -0600

    Create lab 3.1.1.10.py

---
+++ Unidad 1/modulo 3/lab 3.1.1.10.py
@@ -0,0 +1,10 @@
+# Autor: Kevin Cabrera Luna
+# LAB 3.1.1.10
+# FECHA 2 SEP
+planta = input("Ingrese el nombre de la planta: ")
+if planta == "ESPATIFILIO":
+    print("Si, ¡El ESPATIFILIO! es la mejor planta de todos los tiempos!")
+elif planta == "espatifilo":
+    print("No, ¡quiero un gran ESPATIFILIO!")
+else:
+    print(f"¡ESPATIFILIO!, ¡No {planta}!")
