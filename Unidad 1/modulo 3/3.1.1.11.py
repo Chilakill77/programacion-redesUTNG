3b4f0ed9215ca5a77b09374a9cc54eeedb700f86
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-10-02 10:25:36 -0600

    Add files via upload

---
+++ Unidad 1/modulo 3/3.1.1.11.py
@@ -0,0 +1,11 @@
+﻿# autor: Kevin Cabrera Luna
+# Lab 3.1.1.11
+# fecha: 2 oct
+income = float(input("Introduce el ingreso anual:"))
+if income<= 85528:
+    tax = round(income * 0.18)- 556.02
+else:
+    tax = 14839.02 + (income - 85528) * 0.32
+    tax = round(max(tax,0),0)
+    tax= round(tax,0)
+print("El impuesto es:", tax, "pesos")
