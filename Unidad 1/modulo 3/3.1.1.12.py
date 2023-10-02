56fb74c84039d209e0f766545602f1c011a0ddec
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-10-02 10:43:01 -0600

    Add files via upload

---
+++ Unidad 1/modulo 3/3.1.1.12.py
@@ -0,0 +1,15 @@
+﻿# Autor: Kevin Cabrera Luna
+# Laboratorio 3.1.1.12
+# 2 de oct
+year = int(input("Introduce un año: "))
+if year < 1582:
+    print("No dentro del período del calendario Gregoriano")
+else:
+    if (year % 4 != 0):
+        print("Año Común")
+    elif (year % 100 != 0):
+        print("Año Bisiesto")
+    elif (year % 400 != 0):
+        print("Año Común")
+    else:
+        print("Año Bisiesto")
