bd2117ea9ea22fae7f436fda825dfbdb1e1282e0
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-10-02 10:19:47 -0600

    Update lab 3.1.1.4.py

--- Unidad 1/modulo 3/lab 3.1.1.4.py
+++ Unidad 1/modulo 3/lab 3.1.1.4.py
@@ -1,7 +1,7 @@
-/////
-    Autor: Kevin Cabrera Luna
-    lab 3.1.1.4
-    fecha:2 de sep
-    /////
+#
+#   Autor: Kevin Cabrera Luna
+#    lab 3.1.1.4
+#    fecha:2 oct
+#
 n = int(input("Ingrese un número: "))
 print(n >= 100)
