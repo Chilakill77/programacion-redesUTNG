6be1f315a275e3c59c21ea0c0969a20bb70c1251
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-10-11 23:24:32 -0600

    Create 3.2.1.9.py

---
+++ Unidad 1/modulo 3/3.2.1.9.py
@@ -0,0 +1,6 @@
+#Kevin Cabrera Luna
+while True:
+    palabra = input("Ingresa una palabra: ")
+    if palabra.lower() == "chupacabra":
+        print("¡Has dejado el bucle con éxito.")
+        break  # Salir del bucle si la palabra ingresada es "chupacabra"
