161c0c7a311f6e01e2a5d64c02d5d955ffced1ad
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-10-11 22:39:36 -0600

    Create 2.4.1.7.py

---
+++ Unidad 1/modulo 2/2.4.1.7.py
@@ -0,0 +1,17 @@
+*****Kevin Cabrera Luna
+***2.4.1.7
+
+juan = 7
+maria = 8
+adan = 6
+print(juan, maria, adan)
+total_manzanas = juan + maria + adan
+print("Número Total de Manzanas:", total_manzanas)
+suma = juan + maria
+resta = adan - juan
+multiplicacion = juan * maria
+division = adan / maria
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
