35dc00610eea95b19128325c274b4b6f1b520342
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-10-12 00:14:19 -0600

    Create 3.4.1.6.py

---
+++ Unidad 1/modulo 3/3.4.1.6.py
@@ -0,0 +1,6 @@
+#Kevin Cabrera Luna
+sombrero = [1, 2, 3, 4, 5]
+sombrero[2] = int(input("Ingrese un número entero para reemplazar el número central: "))
+del sombrero[-1]
+print("Longitud de la lista:", len(sombrero))
+
