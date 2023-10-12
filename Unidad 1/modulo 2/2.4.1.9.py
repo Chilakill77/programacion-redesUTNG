72eed73018efd560a1f8f95fcc97076884da787d
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-10-11 22:42:34 -0600

    Create 2.4.1.9.py

---
+++ Unidad 1/modulo 2/2.4.1.9.py
@@ -0,0 +1,8 @@
+*****Kevin Cabrera Luna
+*** 2.4.1.9
+millas = 7.38
+kilometros = 12.25
+millas_en_kilometros = millas * 1.61
+print(millas, "millas son", round(millas_en_kilometros, 2), "kilómetros")
+kilometros_en_millas = kilometros / 1.61
+print(kilometros, "kilómetros son", round(kilometros_en_millas, 2), "millas")
