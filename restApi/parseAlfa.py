4faaf74fc78dcaa2106373ce2c2f6b21c822854e
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-11-16 17:57:53 -0600

    Add files via upload

---
+++ restApi/parseAlfa.py
@@ -0,0 +1,18 @@
+#Kevin
+import requests
+api_key = "TGFSI2VP3ZL0SVZB"
+
+while True:
+    symbol = input("Ingrese el símbolo de la acción que desea consultar (o 'exit' para salir): ")
+    if symbol.lower() == 'exit':
+        print("Saliendo del programa.")
+        break 
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}'
+    print(f"URL de la solicitud: {url}")
+    r = requests.get(url)
+    if r.status_code == 200:
+        data = r.json()
+        print(data)
+    else:
+        print(f"Error en la solicitud. Código de estado: {r.status_code}")
+        print(r.text)
