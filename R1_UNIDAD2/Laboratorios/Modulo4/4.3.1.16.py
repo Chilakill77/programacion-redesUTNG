8781a459a384198a9fe4a7192bb6c4497e06ebee
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-11-23 13:28:08 -0600

    Update 4.3.1.16.py

--- R1_UNIDAD2/Laboratorios/Modulo4/4.3.1.16.py
+++ R1_UNIDAD2/Laboratorios/Modulo4/4.3.1.16.py
@@ -11,19 +11,7 @@ def contar_letras(entrada):
             print(f"{letra} -> {frecuencia}")
     except Exception as e:
         print(f"Se produjo un error: {e}")
+
 cadena_entrada = input("Ingrese la cadena de entrada: ")
 contar_letras(cadena_entrada)
-def contar_letras(entrada):
-    try:
-        contenido = entrada.lower()
-        recuentos_letras = {}
-        for caracter in contenido:
-            if caracter.isalpha() and caracter.isascii():
-                recuentos_letras[caracter] = recuentos_letras.get(caracter, 0) + 1
-        recuentos_ordenados = sorted(recuentos_letras.items(), key=lambda x: x[1], reverse=True)
-        for letra, frecuencia in recuentos_ordenados:
-            print(f"{letra} -> {frecuencia}")
-    except Exception as e:
-        print(f"Se produjo un error: {e}")
-cadena_entrada = input("Ingrese la cadena de entrada: ")
-contar_letras(cadena_entrada)
+
