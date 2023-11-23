2e52c8a18369a163fa96db5ca72ba4472fea5b7f
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-11-23 12:02:01 -0600

    Create 2.3.1.18.py

---
+++ R1_UNIDAD2/Laboratorios/modulo2/2.3.1.18.py
@@ -0,0 +1,10 @@
+#Kevin Cabrera Luna
+#R1 u2
+def mysplit(strng):
+    words = strng.split()
+    return words
+print(mysplit("Ser o no ser, esa es la pregunta"))
+print(mysplit("Ser o no ser,esa es la pregunta"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
