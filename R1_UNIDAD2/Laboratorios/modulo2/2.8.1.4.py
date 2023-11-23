e08e567e98650360dfef0d71cccc7753aaf398a6
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-11-23 13:17:05 -0600

    Update 2.8.1.4.py

--- R1_UNIDAD2/Laboratorios/modulo2/2.8.1.4.py
+++ R1_UNIDAD2/Laboratorios/modulo2/2.8.1.4.py
@@ -9,6 +9,7 @@ def obtener_valor_en_rango(minimo, maximo):
             else:
                 print(f'Error: el valor no está dentro del rango permitido ({minimo}..{maximo})')
         except ValueError:
-            print('Error: entrada incorrecta')
+            print('Error: entrada incorrecta, por favor ingresa un número entero.')
+
 valor = obtener_valor_en_rango(-10, 10)
 print(f'El número es: {valor}')
