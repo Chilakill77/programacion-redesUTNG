9faf6e2457cbcc1f9e568091c757583962826a7e
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-11-23 12:20:58 -0600

    Create 4.4.1.8.py

---
+++ R1_UNIDAD2/Laboratorios/Modulo4/4.4.1.8.py
@@ -0,0 +1,18 @@
+#Kevin Cabrera Luna
+import os
+def find(path, target_dir):
+    current_dir = os.path.abspath(path)
+    if os.path.basename(current_dir) == target_dir:
+        print(current_dir)
+    try:
+        dir_contents = os.listdir(current_dir)
+    except FileNotFoundError:
+        print(f"El directorio {current_dir} no existe.")
+        return
+    for item in dir_contents:
+        item_path = os.path.join(current_dir, item)
+        if os.path.isdir(item_path):
+            find(item_path, target_dir)
+path = "./tree"
+target_directory = "python"
+find(path, target_directory)
