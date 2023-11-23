92279b1420232fdd1bc64b2116dbadff69f06fad
Chilakill77 <138149853+Chilakill77@users.noreply.github.com>
2023-11-23 13:20:10 -0600

    Update 3.2.1.16.py

--- R1_UNIDAD2/Laboratorios/MODULO3/3.2.1.16.py
+++ R1_UNIDAD2/Laboratorios/MODULO3/3.2.1.16.py
@@ -2,27 +2,34 @@
 #R1 U2
 class QueueError(Exception):
     pass
+
 class Queue:
     def __init__(self):
         self.items = []
+
     def is_empty(self):
         return len(self.items) == 0
+
     def put(self, item):
         self.items.append(item)
+
     def get(self):
         if not self.is_empty():
             return self.items.pop(0)
         else:
             raise QueueError("Queue is empty")
+
 class SuperQueue(Queue):
-    def isempty(self):
+    def isempty(self):  # Se corrigió el nombre del método a 'isempty'
         return self.is_empty()
+
 que = SuperQueue()
 que.put(1)
 que.put("perro")
 que.put(False)
+
 for i in range(4):
-    if not que.is_empty():  # Corregir aquí
+    if not que.isempty():  # Corregir aquí
         print(que.get())
     else:
         print("Cola vacía")
